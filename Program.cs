f4ef3c78609022af3e9c4e20276469e89960cab4
Benia Morgan <bmorganware@gmail.com>
2024-11-07 09:03:03 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] nums = { 10, 2330, 112233, 12, 949, 3764, 2942, 523863 };
+
+int minimum = nums.Min();
+int maximum = nums.Max();
+int minValue = nums.Where(x => x < 10000).Max();
+int maxValue = nums.Where(x => x > 10 && x < 100).;
